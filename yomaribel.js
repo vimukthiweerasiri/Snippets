74c284f5d1ea9d556ea40704b9d28fa88639701d
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-07-18 01:46:04 -0700

    lol

--- yomaribel.js
+++ yomaribel.js
@@ -3,7 +3,7 @@ var last_showed = 0;
 setInterval(function() {
     var last_active = document.getElementsByClassName("_2v6o")[0].innerText.split(" ")[1].toLowerCase()
     document.getElementsByClassName("_2v6o")[0].innerHTML
-    if (last_active === "1m" || last_active == "now") {
+    if (last_active === "1m" || last_active === "now") {
         var time_now = new Date().getTime();
         if (time_now - last_showed > 60 * 1000) {
             new Notification("Go Go Go\n" + document.getElementsByClassName("_3oh-")[0].innerText.split(" ")[0] + " is active now!");
